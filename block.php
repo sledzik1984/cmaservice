f82938840b4181f5723011f2effe2f5055231d34
Piotr Górski <piotr@prokocim.org>
2013-05-07 22:15:57 +0200

    Popraweczki

---
+++ block.php
@@ -0,0 +1,5 @@
+Query:
+
+
+
+SELECT * FROM `fixture_blocks` WHERE (`block_stop` > '1368386571') AND (`block_start` < '1368472971');
--- listasztuk.php
+++ listasztuk.php
@@ -1,5 +1,10 @@
 <?php
 
+setlocale(LC_TIME, "pl_PL.UTF-8");
+
+//Zmienne post 
+
+
 include 'mysql.php';
 
 echo "<html>\n";
@@ -13,84 +18,150 @@ echo "\t<body>\n";
 
 echo "<div class=\"mainmargin\">\n";
 
-echo "<h1>Lista sztuk</h1>\n";
+echo "<h1>Lista wypożyczeń</h1>\n";
 
 
 echo "<table cellpadding=\"4\" width=\"100%\">\n";
 
 echo "<tr class=\"dark\">\n";
-echo "<td class=\"fleftu\">Klient</td>\n";
-echo "<td class=\"fbt\">Miejsce</td>\n";
-echo "<td class=\"fbt\">Odpowiedzialny</td>\n";
-echo "<td class=\"fbt\">Magazynier</td>\n";
-echo "<td class=\"fbt\">Wydanie</td>\n";
-echo "<td class=\"fbt\">Powrót</td>\n";
-
+echo "<td class=\"fleftu\">Imię</td>\n";
+echo "<td class=\"fbt\">Nazwisko</td>\n";
+echo "<td class=\"fbt\">Firma</td>\n";
+echo "<td class=\"fbt\">Data wydania</td>\n";
+echo "<td class=\"fbt\">Data zwrotu</td>\n";
+echo "<td class=\"fbt\">Wydane</td>\n";
+echo "<td class=\"fbt\">Zwrócone</td>\n";
 echo "<td class=\"frightu\"></td>\n";
 echo "</tr>\n";
 
 
+$query = "SELECT * FROM `rental_details` ORDER BY `id` ASC";
+$result = mysql_query($query);
 
-$q = "SELECT * FROM `events` WHERE `deleted` != '1' ORDER BY `id`";
-$r = mysql_query($q);
-
-
-while ($row = mysql_fetch_assoc($r)) {
+while ($row = mysql_fetch_assoc($result)) {
 
     $id = $row["id"];
-    $start = $row["timestart"];
-    $stop = $row["timestop"];
-    $who = $row["who"];
+    $rental_client = $row["rental_client"];
+    $timestart = strftime("%c",$row["timestart"]);
+    $timestop = strftime("%c",$row["timestop"]);
+    $go = $row["go"];
     $back = $row["back"];
-    $where = $row["where"];
-    $response = $row["response"];
-    $manager = $row["warehousemanager"];
 
-	//Pobieramy z bazy dane klienta:
-
-	$client_query = "SELECT `company` FROM `rental_clients` WHERE `id` = '$who' LIMIT 1";
-	$client_result = mysql_query($client_query);
-
-	if ($debug == 1) { echo "DEBUG: mysql_error():".mysql_error(); } 
-
-	while ($client_row = mysql_fetch_assoc($client_result)) {
-
-		$client_name = $client_row["company"];
-
-
-	}
-
-
+    
     if ($id%2 == 0) {
     //Pętla if odpowiedzialna za rozróżnianie id parzystych i nie parzystych
 
-	echo "<tr onmouseover=\"addClass(this, 'highlight')\" onmouseout=\"removeClass(this, 'highlight')\" class=\"light\">\n";
-	echo "\t<td class=\"fleft\">" . $client_name . "</td>\n";
-	echo "\t<td>" .$where  . "</td>\n";
-	echo "\t<td>" . $response . "</td>\n";
-	echo "\t<td>" . $manager . "</td>\n";
-	echo "\t<td>" . $start . "</td>\n";
-	echo "\t<td>" . $stop . "</td>\n";
-	echo "\t<td class=\"fright\"><a href=\"eventdelete.php?id=".$id. "\" OnClick=\"return confirmLink(this, 'Czy jesteś pewien, że chcesz usunąć sztukę z bazy?');\"><img src=\"img/delete.gif\"></a>&nbsp;<img src=\"img/edit.gif\"></td>\n";
-	echo "</tr>\n\n";
+//    echo "<td onClick=\"top.location.href='/cma_service/?m=historiaurzadzen&b=" . $barcode . "';\" class=\"fleft\" valign=\"top\">\n";
+//        echo $name;
+//            echo "</td>\n";
+            
+
+    //Pobranie danych klienta:
+
+    
+    $query_client = "SELECT * FROM `rental_clients` WHERE `id` = '$rental_client' LIMIT 1";
+    $result_client = mysql_query($query_client);
+    
+    $row_client = mysql_fetch_row($result_client);
+    
+    $firstname = $row_client[1];
+    $lastname = $row_client[2];
+    $company = $row_client[3];
+    
+    
+	echo "<tr class=\"light\">\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\"  class=\"fleft\">" . $firstname . "</td>\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\">" . $lastname . "</td>\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\">" . $company . "</td>\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\">" . $timestart . "</td>\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\">" . $timestop . "</td>\n";
+        
 
     
     
+	if ($go == 0) { 
+	
+	    echo "\t<td><form><input type=\"checkbox\" disabled=\"disabled\"></form></td>\n";
+        
+        } else {
+        
+	    echo "\t<td><form><input type=\"checkbox\" disabled=\"disabled\" checked=\"checked\"></form></td>\n";
+        
+        }
+
+
+        if ($back == 0) {
+        
+    	    echo "<td><form><input type=\"checkbox\" disabled=\"disabled\"></form></td>\n";
+        
+        } else {
+        
+    	    echo "<td><form><input type=\"checkbox\" disabled=\"disabled\" checked=\"checked\"></form></td>\n";
+        
+        }
+
+
+
+        echo "\t<td class=\"fright\"><a href=\"rental_del.php?id=".$id. "\" OnClick=\"return confirmLink(this, 'Czy jesteś pewien, że chcesz usunąć wypożyczenie bazy?');\"><img src=\"img/delete.gif\"></a>&nbsp;<a href=\"rentalgo.php?rid=".$id."&client_id=".$rental_client."\"><img src=\"img/go.gif\"></a>&nbsp;<img src=\"img/edit.gif\"><a href=\"rentalback.php?rid=".$id."\"><img src=\"img/back.gif\"></a></td>\n";
+        
+         
+        
+        
+        echo "</tr>\n\n";
+
+
 } else {
 
-	echo "<tr onmouseover=\"addClass(this, 'highlight')\" onmouseout=\"removeClass(this, 'highlight')\" class=\"lucid\">\n";
-	echo "\t<td class=\"fleft\">" . $client_name . "</td>\n";
-	echo "\t<td>" . $where . "</td>\n";
-	echo "\t<td>" . $response . "</td>\n";
-	echo "\t<td>" . $manager . "</td>\n";
-	echo "\t<td>" . $start . "</td>\n";
-	echo "\t<td>" . $stop . "</td>\n";
-	echo "\t<td class=\"fright\"><a href=\"eventdelete.php?id=".$id. "\" OnClick=\"return confirmLink(this, 'Czy jesteś pewien, że chcesz usunąć sztukęz bazy?');\"><img src=\"img/delete.gif\"></a>&nbsp;<a href=\"editrentalclient.php?id=".$id."\"><img src=\"img/edit.gif\"></a></td>\n";
-	echo "</tr>\n\n";
+    $query_client = "SELECT * FROM `rental_clients` WHERE `id` = '$rental_client' LIMIT 1";
+    $result_client = mysql_query($query_client);
+    
+    $row_client = mysql_fetch_row($result_client);
+    
 
-    }
+    $firstname = $row_client[1];
+    $lastname = $row_client[2];
+    $company = $row_client[3];
+
+
+
+        echo "<tr class=\"light\">\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\"  class=\"fleft\">" . $firstname . "</td>\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\">" . $lastname . "</td>\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\">" . $company . "</td>\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\">" . $timestart . "</td>\n";
+        echo "\t<td onClick=\"top.location.href='/cma_service/?m=rentaldetails&rid=". $id . "';\">" . $timestop . "</td>\n";
+    
+	if ($go == 0) { 
+	
+	    echo "\t<td><form><input type=\"checkbox\" disabled=\"disabled\"></form></td>\n";
+        
+        } else {
+        
+	    echo "\t<td><form><input type=\"checkbox\" disabled=\"disabled\" checked=\"checked\"></form></td>\n";
+        
+        }
+
+
+        if ($back == 0) {
+        
+    	    echo "<td><form><input type=\"checkbox\" disabled=\"disabled\"></form></td>\n";
+        
+        } else {
+        
+    	    echo "<td><form><input type=\"checkbox\" disabled=\"disabled\" checked=\"checked\"></form></td>\n";
+        
+        }
+
+
+        echo "\t<td class=\"fright\"><a href=\"rental_del.php?id=".$id. "\" OnClick=\"return confirmLink(this, 'Czy jesteś pewien, że chcesz usunąć wypożyczenie z bazy?');\"><img src=\"img/delete.gif\"></a>&nbsp;<a href=\"rentalgo.php?rid=".$id."&client_id=".$rental_client."\"><img src=\"img/go.gif\"></a>&nbsp;<img src=\"img/edit.gif\"><a href=\"rentalback.php?rid=".$id."\"><img src=\"img/back.gif\"></a></td>\n";
+        
+        echo "</tr>\n\n";
+
 
 }
+
+}
+
 
 
 echo "<tr class=\"dark\">\n";
